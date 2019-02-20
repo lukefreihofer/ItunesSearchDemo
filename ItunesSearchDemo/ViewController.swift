ae442952bb88b6a9205e8d8e7c9143ed6c1de281
Luke Freihofer <luke.freihofer@gmail.com>
2019-02-20 17:10:07 -0500

    initial commit

---
+++ ItunesSearchDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItunesSearchDemo
+//
+//  Created by Luke Freihofer on 2/20/19.
+//  Copyright © 2019 Luke Freihofer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ItunesSearchDemoTests/ItunesSearchDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ItunesSearchDemoTests.swift
+//  ItunesSearchDemoTests
+//
+//  Created by Luke Freihofer on 2/20/19.
+//  Copyright © 2019 Luke Freihofer. All rights reserved.
+//
+
+import XCTest
+@testable import ItunesSearchDemo
+
+class ItunesSearchDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
